c9363fd3df86eaa2f8423dee8d993cbf69cac7c9
Baptiste MATHON <baptistemathon@MacBook-Pro-de-Baptiste.local>
2025-03-27 18:06:45 +0100

    finition accueil

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -13,22 +13,63 @@ function Accueil() {
             </div>
         </div>
 
-        <div class="search">
-            <div class="search-ville">
-                <p>Ville</p>
-                <input type="text" placeholder='Rechercher une ville'/>
+        <div class="search-border">
+            <div class="search">
+                <div class="search-ville">
+                    <p>Ville</p>
+                    <input type="text" placeholder='Rechercher une ville'/>
+                </div>
+                <div class="search-debut">
+                    <p>Début</p>
+                    <input type="date" placeholder='Date de début'/>
+                </div>
+                <div class="search-fin">
+                    <p>Fin</p>
+                    <input type="date" placeholder='Date de fin'/>
+                </div>
+                <div class="search-button">
+                    <img src="/loop.png" alt="Rechercher"/>
+                </div>
             </div>
-            <div class="search-debut">
-                <p>Début</p>
-                <input type="date" placeholder='Date de début'/>
+        </div>
+
+        <div class="filters">
+            <div>
+                <img src="/euros.png" width={"25px"}/>
+                <p>Prix</p>
             </div>
-            <div class="search-fin">
-                <p>Fin</p>
-                <input type="date" placeholder='Date de fin'/>
+            <div>
+                <img src="/marque.png" width={"25px"}/>
+                <p>Marque</p>
             </div>
-            <div class="search-button">
-                <img src="/loop.png" alt="Rechercher"/>
+            <div>
+                <img src="/car.png" width={"25px"}/>
+                <p>Modèle</p>
             </div>
+            <div>
+                <img src="/fuel.png" width={"25px"}/>
+                <p>Carburant</p>
+            </div>
+            <div>
+                <img src="/gearbox.png" width={"25px"}/>
+                <p>Boîte de vitesse</p>
+            </div>
+            <div>
+                <img src="/door.png" width={"25px"}/>
+                <p>Nombre de portes</p>
+            </div>
+            <div>
+                <img src="/movie.png" width={"25px"}/>
+                <p>Univers</p>
+            </div>
+            <div>
+                <img src="/hbo.png" width={"25px"}/>
+                <p>Franchise</p>
+            </div>
+            <span class="filter">
+                <img src="/filter.png" width={"25px"}/>
+                <p>Filtres</p>
+            </span>
         </div>
 
         <Cars/>
